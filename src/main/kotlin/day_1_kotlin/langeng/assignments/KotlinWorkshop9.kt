e56c6eba2297d8c4b856fbef6a56801b0a98aba0
pafnat <lepeev.pavel@gmail.com>
2020-11-01 23:07:02 +0300

    workshop9 update

--- src/main/kotlin/day_1_kotlin/langeng/assignments/KotlinWorkshop9.kt
+++ src/main/kotlin/day_1_kotlin/langeng/assignments/KotlinWorkshop9.kt
@@ -1,41 +1,26 @@
 package day_1_kotlin.langeng.assignments
 
 // workshop #9 - generics
-/*
- * Класс программиста, который может учить и забывать концепты на определенном языке.
- */
-
 fun main() {
-    val p = Programmer<Kotlin>()
 
-    //TODO: uncomment all
-//    p.learn(Kotlin("basics"))
-//
-//    p.learn(Kotlin("generics"))
-//    p.learn(Kotlin("coroutines"))
+    val p = ProgrammerDone<Kotlin>()
 
-    println(p.howManyConceptsDoIKnow())
+    p.learn(Kotlin("basics"))
+
+    p.learn(Kotlin("generics"))
+    p.learn(Kotlin("coroutines"))
+
     // should be equal to 3
-
-    println(p.lastConcept())
-    //should be Kotlin(element=coroutines)
-
-//    p.forget(Kotlin("generics"))
     println(p.howManyConceptsDoIKnow())
-    // should be equal to 2
+
+    //should be Kotlin(element=coroutines)
+    println(p.lastConcept())
+
+    p.forget(Kotlin("generics"))
+    // should be equal to 3
+    println(p.howManyConceptsDoIKnow())
 }
 
-
-class Programmer<T: Language>() {
-
-    fun howManyConceptsDoIKnow(): Int = TODO()
-
-    // TODO: add "learn" function with new concept
-
-    //TODO: add "forget" function with one concept to forget
-
-    fun lastConcept(): T = TODO()
-}
 
 interface Language{
     val element: String
@@ -45,3 +30,21 @@ data class JavaLanguage(override val element: String) : Language
 data class Kotlin(override val element: String): Language
 data class Swift(override val element: String): Language
 data class C_Sharp(override val element: String): Language
+
+
+class ProgrammerDone<T: Language>() {
+
+    private val concepts = mutableListOf<T>()
+
+    fun howManyConceptsDoIKnow(): Int = concepts.size
+
+    fun learn(concept: T) {
+        concepts.add(concept)
+    }
+
+    fun forget(concept: T) {
+        concepts.remove(concept)
+    }
+
+    fun lastConcept(): T = concepts.last()
+}
